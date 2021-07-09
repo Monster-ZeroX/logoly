e45d8549eb59eb7ddde7860f08f0bd774220414b
Mⷨoͦns͛ᴛⷮeͤrͬ Zeͤrͬoͦ <69518719+Monster-ZeroX@users.noreply.github.com>
2021-07-09 12:38:47 +0530

    Update Author.vue

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -19,18 +19,3 @@ h2
     align-items center
     padding 50px 0
 </style>
-
-<template>
-    <div class="author">
-        <div class="info">
-            <img class="avatar" src="../assets/avatar.png" alt="bestony">
-            <h2> Bestony</h2>
-            <h4>An indie developer / Focus on something interesting.</h4>
-        </div>
-        <div class="social">
-            <a class="github" href="https://github.com/bestony"><i class="iconfont icon-github"></i> GitHub</a>
-            <a class="twitter" href="https://twitter.com/xiqingongzi"><i class="iconfont icon-twitter"></i> Twitter</a>
-            <a class="gmail" href="mailto:xiqingongzi+logoly@gmail.com"><i class="iconfont icon-gmail"></i> Email</a>
-        </div>
-    </div>
-</template>
